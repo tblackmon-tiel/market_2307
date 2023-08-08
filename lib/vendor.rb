2528cee2f398f83478aafca28ee2bfcae6b12696
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-08-08 09:51:00 -0600

    feat: add potential_revenue to Vendor

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -13,4 +13,10 @@ class Vendor
   def stock(item, amount)
     @inventory[item] += amount
   end
+
+  def potential_revenue
+    @inventory.sum do |item, amount|
+      amount * item.price
+    end
+  end
 end
